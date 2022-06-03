9086df5c8a62841cff57e9ce4e2a1595e2e1c46e
Brian <b.perez3237@gmail.com>
2022-06-02 20:56:51 -0400

    Add first fetch request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+    fetch("http://localhost:9292/activities")
+      .then((r) => r.json())
+      .then((data) => console.log(data));
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
