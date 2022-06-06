651ecd526e90455d2218a13b10de42182ebafe2b
Brian <b.perez3237@gmail.com>
2022-06-05 23:36:32 -0400

    Update home page styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,9 +10,9 @@ function Home() {
     return(
         <Container>
             <Row><h1 className="d-flex justify-content-center">Project Planner</h1></Row>
-            <Stack>
-                <Button variant="dark" size="lg"><NavLink to="/schedule">Schedule</NavLink></Button>
-                <Button variant="dark" size="lg"><NavLink to="/update-costs">Update Costs</NavLink></Button>
+            <Stack >
+                <NavLink className="d-flex justify-content-center" to="/schedule">Schedule</NavLink>
+                <NavLink className="d-flex justify-content-center" to="/update-costs">Update Costs</NavLink>
             </Stack>
         </Container>
     )
